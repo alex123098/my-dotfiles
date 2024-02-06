d3f0fad6fd45647bb60020422cebadc656cb8988
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2024-02-06 19:14:12 +0200

    NVim: make LSP work with zsh scripts

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- treat zsh files as shell scripts
+vim.filetype.add({
+  extension = {
+    zsh = "sh",
+    sh = "sh",
+  },
+  filename = {
+    [".zshrc"] = "sh",
+    [".zshenv"] = "sh",
+  },
+})
