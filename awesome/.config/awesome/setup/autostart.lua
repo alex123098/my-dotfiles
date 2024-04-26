96ba165e21e40d42ba3d83a52fbafa47aae1fd70
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2024-04-27 01:43:48 +0300

    incremental

--- awesome/.config/awesome/setup/autostart.lua
+++ awesome/.config/awesome/setup/autostart.lua
@@ -5,6 +5,7 @@ local function run_once(cmd_arr)
 	end
 end
 
+awful.spawn.with_shell("bright apply")
 run_once({ "hsetroot", "-solid", "#000066" })
 run_once({ "xcompmgr", "-c", "-C", "-t-5", "-l-5", "-r4.2", "-o.55" })
 run_once({ "/usr/lib/xfce-polkit/xfce-polkit" })
--- bright_up.sh
+++ bright_up.sh
@@ -2,12 +2,21 @@
 
 MON=$(xrandr | grep " connected" | cut -f 1 -d " ")
 STEP=0.05
+CUR_BR_PATH="${XDH_DATA_HOME:-$HOME/.local/share}/bright_up/cur_value"
 
 BR_STR=$(xrandr --current --verbose | grep ^"$MON" -A5 | tail -n 1)
 CUR_BR="${BR_STR##* }"
 
-REAL=${CUR_BR%%"."*}
-DEC=${CUR_BR#*"."}
+if [[ -z "$1" ]]; then
+	echo "${CUR_BR}"
+	exit 0
+fi
+
+if [[ "$1" == "apply" ]]; then
+	NEW_BR=$(cat "${CUR_BR_PATH}")
+	xrandr --output "${MON}" --brightness "${NEW_BR}"
+	exit 0
+fi
 
 NEW_BR=CUR_BR
 [[ "$1" == "up" || "$1" == "+" ]] && NEW_BR=$(echo "${CUR_BR}" + "${STEP}" | bc -l)
@@ -16,4 +25,6 @@ NEW_BR=CUR_BR
 [[ "${NEW_BR:0:1}" == "1" ]] && NEW_BR=1.0
 [[ $NEW_BR < 0.05 ]] && NEW_BR=0.05
 
+# store new brightness value
+echo "${NEW_BR}" >"${CUR_BR_PATH}"
 xrandr --output "${MON}" --brightness "${NEW_BR}"
