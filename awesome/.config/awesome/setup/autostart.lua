f1b472a6384cd3d7bdf5eff2ac9f1d2acda68391
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2024-05-18 00:01:10 +0300

    awesome: set D-Bus environment upon start

--- awesome/.config/awesome/setup/autostart.lua
+++ awesome/.config/awesome/setup/autostart.lua
@@ -6,6 +6,7 @@ local function run_once(cmd_arr)
 end
 
 awful.spawn.with_shell("bright apply")
+run_once({ "dbus-update-activation-environment", "--all" })
 run_once({ "hsetroot", "-solid", "#000066" })
 run_once({ "xcompmgr", "-c", "-C", "-t-5", "-l-5", "-r4.2", "-o.55" })
 run_once({ "/usr/lib/xfce-polkit/xfce-polkit" })
