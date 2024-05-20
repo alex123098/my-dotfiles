f376c1debe1eb572ecd22d7abcf72f099f6c3463
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2024-05-20 18:23:42 +0300

    vim: remove shellcheck - takes too much resources

--- nvim/.config/nvim/lua/langs/bash.lua
+++ nvim/.config/nvim/lua/langs/bash.lua
@@ -18,15 +18,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "bashls", "bash-debug-adapter", "shellcheck" })
+      vim.list_extend(opts.ensure_installed, { "bashls", "bash-debug-adapter" })
     end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        bash = { "shellcheck" },
-      },
-    },
   },
 }
