9799abf1388ebb7fc8e9633c8c08f2291e0007c4
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2024-05-22 01:26:01 +0300

    delete duplicated completions already included in zsh-completions

--- zsh/.config/zsh/completions.zsh
+++ zsh/.config/zsh/completions.zsh
@@ -1,24 +1,6 @@
-fpath=($ZDOTDIR/completions $fpath)
-
 source /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.zsh
 
 autoload -U +X compinit && compinit
 
 # init fzf integrations
 eval $(fzf --zsh)
-
-# dotnet cli completions
-_dotnet() {
-	local completions=("$(dotnet complete "$words")")
-
-	if [ -z "$completions" ]; then
-		_arguments '*::arguments: _normal'
-		return
-	fi
-	_values = "${(ps:\n:)completions}"
-}
-
-compdef _dotnet dotnet
-
-# aws cli completions
-source /usr/bin/aws_zsh_completer.sh
