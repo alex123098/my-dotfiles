2a9b28466590c08d1dacf3f9f52f57124293dd76
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2025-08-21 20:29:39 +0300

    remove clangd_extensions.cmp_scores completions addon as it breaks nvim_cmp for some reason

--- nvim/.config/nvim/lua/langs/clang.lua
+++ nvim/.config/nvim/lua/langs/clang.lua
@@ -70,14 +70,6 @@ return {
     },
   },
   {
-    "nvim-cmp",
-    opts = function(_, opts)
-      opts.sorting = opts.sorting or {}
-      opts.sorting.comparators = opts.sorting.comparators or {}
-      table.insert(opts.sorting.comparators, 1, require "clangd_extensions.cmp_scores")
-    end,
-  },
-  {
     "mfussenegger/nvim-dap",
     opts = function()
       local dap = require "dap"
