5285c0363634bfe01a7fc6010ed3961bb7e91e60
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2025-08-21 20:32:14 +0300

    configure mini.surround

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -66,6 +66,20 @@ return {
   },
   {
     "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+
+        suffix_last = "l",
+        suffix_next = "n",
+      },
+    },
   },
   {
     "echasnovski/mini.move",
