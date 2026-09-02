bfc3602c962ca257ab7b60785af18c8bff3cbac0
Oleksii Zaichenko <alex.zaychenko0@gmail.com>
2026-09-02 17:22:13 +0300

    feat(nvim): keybinding to opt-out buffer autoformat on save

--- nvim/.config/nvim/lua/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -5,17 +5,24 @@ pack.add { "stevearc/conform.nvim" }
 
 local conform = require "conform"
 
+local enabled = true
+
+k.nmap("<leader>bnf", function()
+  enabled = not enabled
+  if enabled then
+    vim.notify "Autoformat on save: enabled"
+  else
+    vim.notify "Autoformat on save: disabled"
+  end
+end)
+
 conform.setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
     local disable_ft = { c = true, cpp = true }
-    return {
+    return enabled and {
       timeout_ms = 500,
       lsp_fallback = not disable_ft[vim.bo[bufnr].filetype],
-    }
+    } or nil
   end,
 }
-
-k.nmap("<leader>bf", function()
-  conform.format()
-end)
